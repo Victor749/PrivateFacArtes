80da34cf99ccc20a42147c0e4bf5b5561174d097
Berenice <berenice.guerrero@ucuenca.edu.ec>
2020-09-02 12:36:45 -0500

    arreglando metodos de comentarios

--- routes/comentarios.js
+++ routes/comentarios.js
@@ -19,10 +19,10 @@ router.post('/new', function(req, res){
     connection.query(sql_0, function(error0, results0, fields0){
         if(error0){
             debug(error0);
-            return res.send(500);
+            res.sendStatus(500);
         }else if(results0.length == 0){
             // debug(results0);
-            return res.sendStatus(404);
+            res.sendStatus(404);
         }
         // debug(results0);
         const { idUsuario } = results0[0];
@@ -33,10 +33,12 @@ router.post('/new', function(req, res){
         connection.query(sql, function(error, results, fields){
             if(error){
                 debug(error);
-                return res.send(500);
+                res.sendStatus(500);
+                //return res.send(500);
             }
             // debug(results);
-            return res.send(200);
+            //console.log(results);
+            res.send(results);
         });
     });
 
@@ -49,20 +51,20 @@ router.get('/getComentario/:idObra/:actual/:limit/:identifier', function(req, re
     limit = parseInt(req.params.limit);
     identifier = req.params.identifier;
     console.log(identifier);
-    let sql = `select  idComentario, comentario.idUsuario, usuario.nombreUsuario, usuario.identificador, usuario.email, contenido, usuario.linkFoto, comentario.fecha from obra,comentario,usuario where obra.idObra=${req.params.idObra} and comentario.idObra=${req.params.idObra} and comentario.idUsuario = usuario.idUsuario`;
+    let sql = `select  idComentario, comentario.idUsuario, usuario.nombreUsuario, usuario.identificador, usuario.email, contenido, usuario.linkFoto, comentario.fecha from obra,comentario,usuario where obra.idObra=${req.params.idObra} and comentario.idObra=${req.params.idObra} and comentario.idUsuario = usuario.idUsuario order by comentario.idComentario desc limit ${actual}, ${limit}`;
     connection.query(sql, function (error, results) {
       
     if (error) {
       debug(error);
       res.sendStatus(500);
     }else{
-      finIndex = actual+limit;
+     /* finIndex = actual+limit;
       console.log(finIndex)
       if(finIndex >= results.length){
         finIndex = results.length;
-      }
+      }*/
       resultadoC = [];
-      for (var i=actual; i<finIndex;i++){
+      for (var i=0; i<results.length;i++){
         //console.log(results[i].identificador);
         if(!(identifier != undefined && (identifier == results[i].identificador))){
           results[i].idUsuario = 'hide';
@@ -70,6 +72,7 @@ router.get('/getComentario/:idObra/:actual/:limit/:identifier', function(req, re
         }
         resultadoC.push(results[i]);
       }
+      console.log(resultadoC);
       res.send(resultadoC);
     }
   });
@@ -109,6 +112,22 @@ router.delete('/deleteComentario/:idComentario', function(req, res){
       
  });
 
+ router.get('/getNewComentario/:idComentario', function(req, res){
+  // console.log(req.body.contenido);
+   data = req.body;
+   let sql = `select idComentario, comentario.idUsuario, usuario.nombreUsuario, usuario.identificador, usuario.email, contenido, usuario.linkFoto, comentario.fecha from comentario,usuario where comentario.idComentario=${req.params.idComentario} and comentario.idUsuario = usuario.idUsuario `;
+ 
+   connection.query(sql, function(error, result, fields){
+     if(error){
+       debug(error);
+       res.sendStatus(500);
+     }else{
+       console.log(result);
+       res.send(result[0]);
+     }
+   });
+      
+ });
 
 
 
