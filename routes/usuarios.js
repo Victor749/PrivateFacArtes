8871d840526e5a2cd67f00806880ba154fed5a2b
Pablo61098 <pablosolano61098@gmail.com>
2020-09-16 19:12:20 -0500

    Improved the identifier generator

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,7 +6,7 @@ var debug = require('debug')('backendmuseovirtual:usuarios');
 
 function makeid(length) {
     var result           = '';
-    var characters       = 'Vp';
+    var characters       = 'abcdefghijklmnopqrstUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     var charactersLength = characters.length;
     for ( var i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
